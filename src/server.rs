a1b365f3484aa8415b43c3c8ed93216935256441
Malavan Sotheeswaran <malavan@DESKTOP-8LSCSR1.localdomain>
2022-08-29 15:18:16 -0400

    more progress

--- src/server.rs
+++ src/server.rs
@@ -1,44 +1,62 @@
 use std::{
-    io::{prelude::*, BufReader},
+    io::{prelude::*, ErrorKind},
     net::{TcpListener, TcpStream},
     collections::HashMap,
     time::{SystemTime, Duration},
 };
 
+struct Connection {
+    pub stream: TcpStream,
+    pub buf: String,
+}
+
 pub struct Server {
     bind_addr: String,
     key_embed_map: HashMap<String, String>,
-    conns: Vec<TcpStream>,
+    conns: Vec<Connection>,
 }
 
 impl Server {
     pub fn start(mut self) {
         match TcpListener::bind(&self.bind_addr) {
             Ok(listener) => {
+                listener.set_nonblocking(true);
                 loop {
                     let start = SystemTime::now();
                     for _ in 1..100 {
                         match listener.accept() {
                             Ok((stream, client)) => {
                                 println!("Accepted connection from: {client:?}");
-                                self.conns.push(stream);
+                                self.conns.push(Connection {
+                                    stream: stream,
+                                    buf: String::new(),
+                                });
                             },
-                            Err(e) => println!("Error accepting connection: {e:?}")
+                            Err(e) => if e.kind() != ErrorKind::WouldBlock {
+                                println!("Error accepting connection: {e:?}")
+                            }
                         }
                         match start.elapsed() {
                             Ok(elapsed) => if elapsed > Duration::from_millis(1000) { break; },
                             Err(e) => println!("Error checking time spent waiting for conns: {e:?}")
                         }
                     }
-                    for stream in &self.conns {
-                        let buf_reader = BufReader::new(stream);
-                        let received: Vec<_> = buf_reader
-                            .lines()
-                            .map(|result| result.unwrap())
-                            .take_while(|line| !line.is_empty())
-                            .collect();
-
-                        println!("Received: {:#?}", received);
+                    self.conns.retain_mut(|conn| {
+                        let mut deleted = false;
+                        match conn.stream.read_to_string(&mut conn.buf) {
+                            Ok(bytes_read) => if bytes_read == 0 {
+                                deleted = true;
+                            }
+                            Err(e) => {
+                                println!("Error reading from connection {e:?}");
+                                deleted = true;
+                            }
+                        }
+                        !deleted
+                    });
+                    for conn in &self.conns {
+                        let msg = &conn.buf;
+                        println!("Got: {msg:?}");
                     }
                 }
             }
