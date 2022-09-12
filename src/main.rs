2c11a96953184ae2828f27ac31894d782ff47323
Malavan Sotheeswaran <malavan@DESKTOP-8LSCSR1.localdomain>
2022-09-12 05:18:39 -0400

    ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod server;
 
 fn main() {
-    let server = server::Server::new("127.0.0.1:7487".to_string()).expect("Could not create rust_burt model");
+    let server = server::Server::new("127.0.0.1:7487".to_string())
+        .expect("Could not create rust_burt model");
     server.start();
 }
--- src/server.rs
+++ src/server.rs
@@ -3,12 +3,14 @@ const TEMP_READ_BUF_SIZE: usize = 1024;
 const MAX_READ_BUF_SIZE: usize = 4096;
 
 use std::{
+    collections::HashMap,
     io::{prelude::*, ErrorKind},
     net::{TcpListener, TcpStream},
-    collections::HashMap,
 };
 
-use rust_bert::pipelines::sentence_embeddings::{SentenceEmbeddingsBuilder, SentenceEmbeddingsModel, SentenceEmbeddingsModelType};
+use rust_bert::pipelines::sentence_embeddings::{
+    SentenceEmbeddingsBuilder, SentenceEmbeddingsModel, SentenceEmbeddingsModelType,
+};
 
 struct Connection {
     pub stream: TcpStream,
@@ -26,7 +28,9 @@ impl Server {
     pub fn start(mut self) {
         match TcpListener::bind(&self.bind_addr) {
             Ok(listener) => {
-                listener.set_nonblocking(true).expect("Error setting listener to nonblocking");
+                listener
+                    .set_nonblocking(true)
+                    .expect("Error setting listener to nonblocking");
                 //main event loop
                 loop {
                     //accept new clients
@@ -38,9 +42,11 @@ impl Server {
                                     stream: stream,
                                     buf: String::new(),
                                 });
-                            },
-                            Err(e) => if e.kind() != ErrorKind::WouldBlock {
-                                println!("Error accepting connection: {e:?}")
+                            }
+                            Err(e) => {
+                                if e.kind() != ErrorKind::WouldBlock {
+                                    println!("Error accepting connection: {e:?}")
+                                }
                             }
                         }
                     }
@@ -50,14 +56,18 @@ impl Server {
                         if conn.buf.len() > MAX_READ_BUF_SIZE {
                             return false;
                         }
-                        let mut buf : [u8; TEMP_READ_BUF_SIZE] = [0; TEMP_READ_BUF_SIZE];
+                        let mut buf: [u8; TEMP_READ_BUF_SIZE] = [0; TEMP_READ_BUF_SIZE];
                         match conn.stream.read(&mut buf) {
-                            Ok(bytes_read) => if bytes_read == 0 {
-                                return false;
-                            } else {
-                                match std::str::from_utf8(&buf[0..bytes_read]) {
-                                    Ok(read) => conn.buf.push_str(read),
-                                    Err(e) => println!("Error saving data read from client: {e:?}")
+                            Ok(bytes_read) => {
+                                if bytes_read == 0 {
+                                    return false;
+                                } else {
+                                    match std::str::from_utf8(&buf[0..bytes_read]) {
+                                        Ok(read) => conn.buf.push_str(read),
+                                        Err(e) => {
+                                            println!("Error saving data read from client: {e:?}")
+                                        }
+                                    }
                                 }
                             }
                             Err(e) => {
@@ -71,35 +81,39 @@ impl Server {
                     //check client buffers for commands
                     for conn in &mut self.conns {
                         if let Some(idx) = conn.buf.find("\r\n") {
-                            let mut full_command: String = conn.buf.drain(..idx+2).collect::<String>();
+                            let mut full_command: String =
+                                conn.buf.drain(..idx + 2).collect::<String>();
                             full_command.truncate(idx);
-                            if let Some((command,key)) = full_command.split_once(' ') {
+                            if let Some((command, key)) = full_command.split_once(' ') {
                                 match command {
                                     "put" => {
                                         if let Some((key, rest)) = key.split_once(' ') {
                                             println!("Request to set {key:?} to {rest:?}");
                                             match self.model.encode(&[rest]) {
                                                 Ok(embedding) => {
-                                                    self.key_embed_map.insert(String::from(key), format!("{embedding:?}"));
-                                                },
+                                                    self.key_embed_map.insert(
+                                                        String::from(key),
+                                                        format!("{embedding:?}"),
+                                                    );
+                                                }
                                                 Err(e) => {
                                                     println!("Failed computing embedding {e:?}");
                                                     match conn.stream.write("FAIL\r\n".as_bytes()) {
-                                                        Ok(_writen) => {
-            
-                                                        },
-                                                        Err(e) => println!("Error writing to connection {e:?}")
+                                                        Ok(_writen) => {}
+                                                        Err(e) => println!(
+                                                            "Error writing to connection {e:?}"
+                                                        ),
                                                     }
                                                 }
                                             }
                                             match conn.stream.write("OK\r\n".as_bytes()) {
-                                                Ok(_writen) => {
-    
-                                                },
-                                                Err(e) => println!("Error writing to connection {e:?}")
+                                                Ok(_writen) => {}
+                                                Err(e) => {
+                                                    println!("Error writing to connection {e:?}")
+                                                }
                                             }
                                         }
-                                    },
+                                    }
                                     "get" => {
                                         println!("Request for {key:?}");
                                         match self.key_embed_map.get(key) {
@@ -107,42 +121,40 @@ impl Server {
                                                 match conn.stream.write(embed.as_bytes()) {
                                                     Ok(_writen) => {
                                                         match conn.stream.write("\r\n".as_bytes()) {
-                                                            Ok(_writen) => {
-                
-                                                            },
-                                                            Err(e) => println!("Error writing to connection {e:?}")
+                                                            Ok(_writen) => {}
+                                                            Err(e) => println!(
+                                                                "Error writing to connection {e:?}"
+                                                            ),
                                                         }
-                                                    },
-                                                    Err(e) => println!("Error writing to connection {e:?}")
+                                                    }
+                                                    Err(e) => println!(
+                                                        "Error writing to connection {e:?}"
+                                                    ),
                                                 }
                                             }
                                             None => {
                                                 match conn.stream.write("NONE\r\n".as_bytes()) {
-                                                    Ok(_writen) => {
-        
-                                                    },
-                                                    Err(e) => println!("Error writing to connection {e:?}")
+                                                    Ok(_writen) => {}
+                                                    Err(e) => println!(
+                                                        "Error writing to connection {e:?}"
+                                                    ),
                                                 }
                                             }
                                         }
-                                    },
+                                    }
                                     "del" => {
                                         println!("Request to delete {key:?}");
                                         self.key_embed_map.remove(key);
                                         match conn.stream.write("OK\r\n".as_bytes()) {
-                                            Ok(_writen) => {
-
-                                            },
-                                            Err(e) => println!("Error writing to connection {e:?}")
+                                            Ok(_writen) => {}
+                                            Err(e) => println!("Error writing to connection {e:?}"),
                                         }
-                                    },
+                                    }
                                     _ => {
                                         println!("Unknown command: {command:?}");
                                         match conn.stream.write("UNKNOWN COMMAND\r\n".as_bytes()) {
-                                            Ok(_writen) => {
-
-                                            },
-                                            Err(e) => println!("Error writing to connection {e:?}")
+                                            Ok(_writen) => {}
+                                            Err(e) => println!("Error writing to connection {e:?}"),
                                         }
                                     }
                                 }
@@ -153,12 +165,14 @@ impl Server {
                     }
                 }
             }
-            Err(e) => println!("Error checking time spent waiting for conns: {e:?}")
+            Err(e) => println!("Error checking time spent waiting for conns: {e:?}"),
         }
     }
 
     pub fn new(bind_addr: String) -> Result<Server, rust_bert::RustBertError> {
-        let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::DistiluseBaseMultilingualCased)
+        let model = SentenceEmbeddingsBuilder::remote(
+            SentenceEmbeddingsModelType::DistiluseBaseMultilingualCased,
+        )
         .with_device(tch::Device::cuda_if_available())
         .create_model()?;
         Ok(Server {
